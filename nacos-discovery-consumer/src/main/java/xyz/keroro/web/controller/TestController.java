e21884b5b85af7d53b6742b08bb6eb1b4a04878a
Keroro <keroro714@qq.com>
2024-01-29 17:59:23 +0800

    feat(consumer): 增加webClient示例

--- nacos-discovery-consumer/src/main/java/xyz/keroro/web/controller/TestController.java
+++ nacos-discovery-consumer/src/main/java/xyz/keroro/web/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 /**
  * @author wangpeng
@@ -15,13 +17,33 @@ public class TestController {
 
     private final RestTemplate restTemplate;
 
-    public TestController(RestTemplate restTemplate) {
+    private final WebClient.Builder webClientBuilder;
+
+    public TestController(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
         this.restTemplate = restTemplate;
+        this.webClientBuilder = webClientBuilder;
     }
 
+    /**
+     * 1. restTemplate方式调用其他服务
+     * @return res
+     */
     @GetMapping("/test")
     public String test() {
         String res = restTemplate.getForObject("http://nacos-discovery-provider/hello?name=keroro", String.class);
         return "Result: " + res;
+    }
+
+    /**
+     * 2. webClient方式调用其他服务
+     * @return res
+     */
+    @GetMapping("/webClientTest")
+    public Mono<String> webClientTest() {
+        return webClientBuilder.build()
+                .get()
+                .uri("http://nacos-discovery-provider/hello?name=keroro")
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
