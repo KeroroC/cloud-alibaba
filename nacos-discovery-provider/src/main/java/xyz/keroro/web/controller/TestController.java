488a004314fca9627ca98f542f5cdfd78a5dc075
Keroro <keroro714@qq.com>
2024-02-17 17:01:20 +0800

    feat(provider): 接入Sentinel

--- nacos-discovery-provider/src/main/java/xyz/keroro/web/controller/TestController.java
+++ nacos-discovery-provider/src/main/java/xyz/keroro/web/controller/TestController.java
@@ -2,6 +2,7 @@ package xyz.keroro.web.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @GetMapping("/test")
+    public String testConnect() {
+        return applicationName;
+    }
 
     @GetMapping("/hello")
     public String hello(String name) {
